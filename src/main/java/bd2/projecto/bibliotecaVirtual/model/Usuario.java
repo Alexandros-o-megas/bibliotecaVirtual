05a1fdbadbe97f1ca9f946904e9d73349ab7e70d
Alexandros-o-megas <quivealexandre@gmail.com>
2025-11-19 12:30:25 +0200

    consertando a falha do Alex

--- src/main/java/bd2/projecto/bibliotecaVirtual/model/Usuario.java
+++ src/main/java/bd2/projecto/bibliotecaVirtual/model/Usuario.java
@@ -3,30 +3,34 @@ package bd2.projecto.bibliotecaVirtual.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "exemplar")
+@Table(name = "usuario")
 @Getter
 @Setter
-public class Exemplar {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id_exemplar;
-    
-    private String codigo;
-    private String tipo; // FISICO, DIGITAL
-    private String formato; // IMPRESSO, PDF, EPUB, AUDIOBOOK
-    private String localizacao;
-    private String estado; // DISPONIVEL, EMPRESTADO, RESERVADO, PERDIDO, MANUTENCAO
-    private String regrasAcesso; // ABERTO, RESTRITO, SO_LEITURA
-    
-    @ManyToOne
-    @JoinColumn(name = "id_livro")
-    private Livro livro;
-    
-    @OneToMany(mappedBy = "exemplar")
+    private Integer id_usuario;
+
+    private String nome;
+    private String email;
+    private Integer idade;
+    private String genero;
+    private String profissao;
+    private String rua;
+    private String bairro;
+    private String quarteirao;
+    private String role; // USUARIO, BIBLIOTECARIO, ADMIN
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<TelefoneUsuario> telefones = new HashSet<>();
+
+    @OneToMany(mappedBy = "usuario")
     private Set<Emprestimo> emprestimos = new HashSet<>();
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<Reserva> reservas = new HashSet<>();
 }
